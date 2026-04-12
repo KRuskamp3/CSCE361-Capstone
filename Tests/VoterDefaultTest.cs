782bd2e5c699da93c769e28d180a0c0dbfe23244
Defi <89351972+defimal@users.noreply.github.com>
2026-04-12 13:36:03 -0500

    Add voter default test

---
+++ Tests/VoterDefaultTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+public class VoterDefaultTest
+{
+    [Fact]
+    public void NewVoter_HasExpectedDefaults()
+    {
+        var voter = new Voter();
+
+        Assert.True(voter.Registered);
+        Assert.False(voter.HasVoted);
+    }
+}
