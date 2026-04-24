52560c1d83f7032db119bc17ea143888320b39f7
Kyson Ruskamp <kyson.ruskamp@gmail.com>
2026-04-24 14:09:13 -0500

    Created framework for accessing SQL database

---
+++ sql-accessor.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Data.SqlClient;
+
+public class sqlAccessor
+{
+    /*
+    Add variables to access DB here private
+
+    examples: 
+    private string connectionString = "Server=myServerAddress;
+    Database=myDataBase;
+    UserId=myUsername;
+    Password=myPassword;";
+    */
+
+    /**
+    Checks if the username exists. If the username exists, check if the password
+     is correct. If the password is correct, return true, otherwise return false.
+     */
+    public bool AttemptLogin(string username, string password) {
+        // TODO: Implement logic
+    }
+    
+}
