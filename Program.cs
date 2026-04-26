b5f646036038a5f4fd6b96b8a8de252fe7e9c261
tnguyen212 <tnguyen212@huskers.unl.edu>
2026-04-25 21:45:09 -0500

    Refactor Program.cs to use WebApplication builder

--- Program.cs
+++ Program.cs
@@ -1,37 +1,21 @@
-using Microsoft.Extensions.Configuration;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+builder.Services.AddCors(options =>
 {
-    static void Main()
+    options.AddPolicy("_MyAllowSubdomainPolicy", policy =>
     {
-        var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        
-        var config = builder.Build();
-        var connectionString = config.GetConnectionString("DefaultConnection");
-        
-        var service = new VotingService(connectionString);
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
-        var voter = new Voter
-        {
-            FirstName = "Jane",
-            LastName = "Doe",
-            DateOfBirth = new DateTime(1995, 5, 1),
-            Address = "456 Elm St"
-        };
+builder.Services.AddControllers();
+builder.Services.AddSingleton<VoteModel>();
 
-        service.RegisterVoter(voter);
+var app = builder.Build();
 
-        var token = service.GenerateToken(1, 1);
-
-        bool success = service.CastVote(token.TokenValue, 2);
-
-        var results = service.GetResults(1);
-
-        foreach (var r in results)
-        {
-            Console.WriteLine($"{r.Key}: {r.Value}");
-        }
-    }
-}
+app.UseCors("_MyAllowSubdomainPolicy");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
